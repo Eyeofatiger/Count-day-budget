67d1144e9e3d7b23b4b76bb57fa8f77ad4279ccc
Eyeofatiger <info@jmlb.co.uk>
2019-09-04 10:51:51 +0100

    Add function in code

--- JScript.js
+++ JScript.js
@@ -1,17 +1,26 @@
 'use strict';
-let money = prompt("What is your monthly budjet?", ''),
-	time = prompt('Enter the date YYYY-MM-DD', '');
 
+let money, time;
+
+function start(){
+        money = +prompt("What is your monthly budjet?", '');
+        time = prompt('Enter the date YYYY-MM-DD', '');
+        while((isNaN(money)) || money == null || money == ""){
+                money = +prompt("What is your monthly budjet?", '');     
+        }
+}
+start();
 let appData = {
 	budget: money,
 	expenses: {},
 	optionalExpenses: {},
 	income: [],
 	timeData: time,
-	savings: false
+	savings: true
 };
 
-for (let i = 0; i<2; i++){
+function chooseExpenses(){
+        for (let i = 0; i<2; i++){
         let a = prompt("Enter the name of most thing which you need to spend money", ''),
             b = prompt("How much will you spend?", ''); 
            if((typeof(a)) === "string" && (typeof(a)) != null && typeof(b) != null
@@ -21,12 +30,18 @@ for (let i = 0; i<2; i++){
            } else{
                    i--;
            }
+           }
+        }
+chooseExpenses();
+
+function detectDayBudget(){
+        appData.moneyPerDay = (appData.budget / 30).toFixed(2);
+        alert("Every day budget : " +appData.moneyPerDay);
 }
 
-appData.moneyPerDay = appData.budget / 30;
+detectDayBudget();
 
-alert("Every day budget : " +appData.moneyPerDay);
-
+function detectLevel(){
         if(appData.moneyPerDay < 100){
                 console.log("It's not so much!!!");
         } else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 500){
@@ -34,5 +49,35 @@ alert("Every day budget : " +appData.moneyPerDay);
         } else if(appData.moneyPerDay > 500){
                 console.log("Very good!");
         } else {console.log("Somthing wrong!");}
+}
+
+detectLevel();
+
+function checkSavings(){
+        if (appData.savings == true){
+                let save = +prompt("How much money do you have in your saving account?"),
+                    percent = +prompt("What interest do you have?");
+                appData.monthIncome = save/100/12*percent;
+                alert("Monthly you have : " + appData.monthIncome + " of interest.");
+        }
+}
+
+checkSavings();
+
+function chooseOptExpenses(){
+        for (let i = 1; i<=3; i++){
+                let ops = prompt("What is your not important expenses?", '');
+                   if((typeof(ops)) === "string" && (typeof(ops)) != null 
+                        && ops != "" && ops.length < 50){ 
+        
+                    appData.expenses[i] = ops;
+                   } else{
+                           i--;
+                   }
+                   }
+
+}
+
+chooseOptExpenses();
 
 
