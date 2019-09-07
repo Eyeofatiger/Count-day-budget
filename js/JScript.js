9d622320b199d4f05a4341e97f2e8593c2a14f40
Eyeofatiger <info@jmlb.co.uk>
2019-09-07 10:27:58 +0100

    Add html, sass  and css files for programs

--- js/JScript.js
+++ js/JScript.js
@@ -1,5 +1,29 @@
 'use strict';
 
+let startBtn = document.getElementById("start"),
+        budgetValue = document.getElementsByClassName('budget-value')[0],
+	dayBudgetValue = document.getElementsByClassName('daybudget-value')[0],
+	levelValue = document.getElementsByClassName('level-value')[0],
+	expensesValue = document.getElementsByClassName('expenses-value')[0],
+	optionalExpensesValue = document.getElementsByClassName('optionalexpenses-value')[0],
+	incomeValue = document.getElementsByClassName('income-value')[0],
+        monthSavingsValue = document.getElementsByClassName('monthsavings-value')[0],
+        yearSavingsValue = document.getElementsByClassName('yearsavings-value')[0],
+
+
+	expensesItem = document.getElementsByClassName('expenses-item'),
+	expensesBtn = document.getElementsByTagName('button')[0],
+	optionalExpensesBtn = document.getElementsByTagName('button')[1],
+        countBtn = document.getElementsByTagName('button')[2],
+        optionalExpensesItem = document.querySelectorAll('.optionalexpenses-item'),
+	incomeItem = document.querySelector('.choose-income'),
+	checkSavings = document.querySelector('#savings'),
+	sumValue = document.querySelector('.choose-sum'),
+        percentValue = document.querySelector('.choose-percent'),
+        yearValue = document.querySelector('.year-value'),
+        monthValue = document.querySelector('.month-value'),
+        dayValue = document.querySelector('.day-value');
+
 let money, time;
 
 function start(){
@@ -42,7 +66,7 @@ let appData = {
                         console.log("It's good, enough money, medium position");
                 } else if(appData.moneyPerDay > 500){
                         console.log("Very good!");
-                } else {console.log("Somthing wrong!");}
+                } else {console.log("Something wrong!");}
                 },
         checkSavings:function (){
                 if (appData.savings == true){
